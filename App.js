83fe219ad08b8298a7315f26097241697b85fc9f
Marcosleal555556 <marcosantoniocordeiro27@gmail.com>
2024-02-21 13:43:13 -0300

    lastcommit

--- App.js
+++ App.js
@@ -2,19 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ backgroundColor: 'red'}}>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+return (
+<View style={styles.container}>
+    
+<Text style={{ backgroundColor: 'white', color: 'blue', textAlign:'right', fontWeight: 'bold', fontSize: 30, borderStyle:'solid' }}>Marcos Antônio Cordeiro Hermes Leal Dos Santos</Text>
+<Text style={{ backgroundColor: 'white', color: 'red', textAlign:'left', fontStyle: 'Italic', fontSize: 25, borderWidth: 1 , borderColor:'black', borderStyle:'solid' }}>Notebook 24</Text>
+<Text style={{ backgroundColor: 'pink', color: 'white', textAlign:'center', fontSize: 20, borderStyle:'solid' }}>Sala 7</Text>
+<Text style={{ backgroundColor: 'green', color: 'white', textAlign:'center', fontSize: 20, borderWidth: 1, borderColor:'red', borderStyle:'solid' }}>Segundo elemento</Text>
+
+</View>
+);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+container: {
     justifyContent: 'center',
-  },
-});
+flex: 1,
+}
+})
